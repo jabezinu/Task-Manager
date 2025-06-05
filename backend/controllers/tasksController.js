e5643929691533ab7c48b77cf718945bd3e9b254
jabezinu <jabezzinu31@gmail.com>
2025-06-05 18:02:32 +0300

    created GET(read) all tasks

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -16,3 +16,19 @@ export const createTask = async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 };
+
+
+// Get all tasks
+export const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+
+        if (!tasks || tasks.length === 0) {
+            return res.status(404).json({ message: 'No tasks found' });
+        }
+        
+        res.json(tasks);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
