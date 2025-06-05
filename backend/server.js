5ce456b613ed13b376c5abd60627b089c354bf0c
jabezinu <jabezzinu31@gmail.com>
2025-06-05 17:53:43 +0300

    created all the routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
+import tasksRoute from './routes/tasksRoute.js';
 
 dotenv.config();
 
@@ -14,6 +15,11 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => {
         console.error('MongoDB connection error:', err);
     });
+
+app.use(express.json());
+
+// Use tasks route
+app.use('/api/tasks', tasksRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
