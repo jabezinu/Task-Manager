fd29449bfe39a956eebb74e0d5079d84356ccdc1
jabezinu <jabezzinu31@gmail.com>
2025-06-05 17:33:21 +0300

    create the modle for the database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log('server is running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 });
